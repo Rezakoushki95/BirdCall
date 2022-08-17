b40ff36746f721c38a0b8e781c1808ad5b61c82c
rezakoushki95 <rezaaarhus@hotmail.com>
2022-08-17 15:54:52 +0200

    Initial Commit

---
+++ BirdCall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirdCall
+//
+//  Created by Reza Koushki on 17/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
