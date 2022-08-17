73a0670a4be3a10fc4dd51c4b2b0ec0edb680c20
rezakoushki95 <rezaaarhus@hotmail.com>
2022-08-17 16:41:22 +0200

    Section 2.15. ex. 1 - A simple app with four buttons matched to four bird call sounds.

--- BirdCall/ViewController.swift
+++ BirdCall/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+	
+	var audioPlayer: AVAudioPlayer!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
+	
+	func playSound(name: String) {
+		if let sound = NSDataAsset(name: name) {
+			do {
+				try audioPlayer = AVAudioPlayer(data: sound.data)
+				audioPlayer.play()
+			} catch {
+				print("Error: \(error.localizedDescription). Could not initialize AVAudioPlayer object")
+			}
+		} else {
+			print("ERROR: Could not read data from file \(name)")
+		}
+	}
 
-
+	@IBAction func easternWhipButton(_ sender: UIButton) {
+		playSound(name: "bird0")
+	}
+	@IBAction func doveButton(_ sender: UIButton) {
+		playSound(name: "bird1")
+	}
+	
+	@IBAction func hawkButton(_ sender: UIButton) {
+		playSound(name: "bird2")
+	}
+	@IBAction func duckButton(_ sender: UIButton) {
+		playSound(name: "bird3")
+	}
 }
 
